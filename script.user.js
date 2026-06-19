acdae7097799b7e49ea2c642b14748bc14f675cc
ShH Y <74806550+1208nn@users.noreply.github.com>
2026-06-19 11:00:28 +0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         浙工大正方导出 ICS 日程
 // @namespace    https://github.com/xlle-er/gdjwkcb_ical
-// @version      4.1.10
+// @version      4.1.11
 // @description  通过对正方教务系统的课表页面的解析，实现导出一个适用于大部分 ics 日历的文件
 // @author       Xiaolele_er & 1208nn (修改自 31415926535x )
 // @supportURL   https://github.com/xlle-er/gdjwkcb_ical/issues
@@ -359,7 +359,7 @@ function examScheduleToICS() {
           ex.timeS,
           ex.timeE,
           ex.course + " " + ex.examName,
-          LocationPrefix + " " + ex.campus + " " + ex.location,
+          LocationPrefix + ex.campus + " " + ex.location,
           ex.seat,
         ),
       );
